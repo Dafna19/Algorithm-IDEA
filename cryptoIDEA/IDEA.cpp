cfe5cd4d608d3a4eeaf1b89319080cc6bc664615
Natalia <stepanishina96@gmail.com>
2016-11-13 12:44:32 +0300

    decoding real size of file

--- cryptoIDEA/IDEA.cpp
+++ cryptoIDEA/IDEA.cpp
@@ -1,7 +1,8 @@
 #include "IDEA.h"
+#include<iostream>
 #define SIZE 128
 
-void IDEA::makeKeys(wchar_t keys[][6], int * bigKey){
+void IDEA::makeKeys(wchar_t keys[][6], int * bigKey) {
 	for (int i = 0; i < 9; i++)
 		for (int j = 0; j < 6; j++)
 			keys[i][j] = 0;
@@ -17,11 +18,12 @@ void IDEA::makeKeys(wchar_t keys[][6], int * bigKey){
 	}
 }
 
-wchar_t IDEA::readBlock(FILE * input){
+wchar_t IDEA::readBlock(FILE * input) {
 	wchar_t buffer = 0, block = 0;
 	for (int i = 0; i < 2; i++) {
 		fread(&buffer, sizeof(unsigned char), 1, input);//1 байт
-		if (feof(input)) return NULL;//достигнут конец
+		if (feof(input) && i == 0) return NULL;//достигнут конец в самом начале
+		if (feof(input) && i == 1) return block;//достигнут конец при считывании второго байта
 		if (i == 1) //считался 2й байт числа
 			buffer = buffer << 8;
 		block += buffer;
@@ -30,21 +32,108 @@ wchar_t IDEA::readBlock(FILE * input){
 	return block;
 }
 
-void IDEA::coding(char * source, char * out, int * bigKey){
+void IDEA::inverseKey() {
+	wchar_t buffer[9][6];
+	for (int i = 0; i < 9; i++) {
+		buffer[i][0] = evclid(65537, key[8 - i][0]);
+		unsigned int test = buffer[i][0] * key[8 - i][0];
+		if (test % 65537 != 1) std::cout << "incorrect evclid(1) " << key[8 - i][0] << " -> " << buffer[i][0] <<" test = "<<test<<" res = "<< (buffer[i][0] * key[8 - i][0]) % 65537 << "\n";
+		if (i == 0 || i == 8) {
+			buffer[i][1] = (0 - key[8 - i][1]) + 65536;///
+			if ((key[8 - i][1] + buffer[i][1]) % 65536 != 0) std::cout << "incorrect inverse\n";
+
+			buffer[i][2] = (0 - key[8 - i][2]) + 65536;///
+			if ((key[8 - i][2] + buffer[i][2]) % 65536 != 0) std::cout << "incorrect inverse\n";
+		}
+		else {
+			buffer[i][1] = (0 - key[8 - i][2]) + 65536;///
+			if ((key[8 - i][2] + buffer[i][1]) % 65536 != 0) std::cout << "incorrect inverse\n";
+
+			buffer[i][2] = (0 - key[8 - i][1]) + 65536;///
+			if ((key[8 - i][1] + buffer[i][2]) % 65536 != 0) std::cout << "incorrect inverse\n";
+		}
+
+		buffer[i][3] = evclid(65537, key[8 - i][3]);
+		test = buffer[i][3] * key[8 - i][3];
+		if (test % 65537 != 1) std::cout << "incorrect evclid " << key[8 - i][3] << " -> " << buffer[i][3] << " test = " << test << " res = " << (buffer[i][3] * key[8 - i][3]) % 65537 << "\n";
+		
+		buffer[i][4] = key[7 - i][4];
+		buffer[i][5] = key[7 - i][5];
+	}
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 6; j++)
+			key[i][j] = buffer[i][j];
+
+}
+
+wchar_t IDEA::evclid(int a, wchar_t b) {
+	//a*x + b*y = 1
+	//std::cout << " a = " << a << " b = " << b;
+	int buf = a;
+	//std::cout << " buf = " << buf;
+	int q, r, x, x1, x2, y, y1, y2;
+	x2 = 1, x1 = 0, y2 = 0, y1 = 1;
+	while (b > 0) {
+		q = a / b;
+		r = a - q * b;
+		x = x2 - q * x1;
+		y = y2 - q * y1;
+		a = b;
+		b = r;
+		x2 = x1;
+		x1 = x;
+		y2 = y1;
+		y1 = y;//
+		//std::cout << " a= " << a << " b= " << b << " q= " << q << " x= " << x << " y= " << y << " r= " << r << " x2= " << x2 << " y2= " << y2 << std::endl;
+	}
+	//std::cout << "  y2 = " << y2;
+	if (y2 < 0) { 
+		//std::cout << "  y2%buf = " << y2%buf << " y2%buf + buf = " << y2%buf + buf;
+		y2 = y2%buf + buf; 
+	}
+	//std::cout << "  return y2 = " << y2%buf << "\n";
+	return y2%buf;//
+}
+
+void IDEA::code(char * source, char * out, int * bigKey, bool decode) {
+
 	FILE *input, *output;
-	wchar_t blockA, blockB, blockC, blockD;
+	unsigned int blockA, blockB, blockC, blockD;
 
 	makeKeys(key, bigKey);
+	if (decode) inverseKey();
+
+	/****************/
+	std::cout << "\nkeys\n";
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 6; j++)
+			std::cout << key[i][j] << " ";
+		std::cout << "\n";
+	}
+	/****************/
 
 	fopen_s(&input, source, "rb");
 	fopen_s(&output, out, "wb");
 
 	while (!feof(input)) {
 		blockA = readBlock(input);
-		blockB = readBlock(input);
-		blockC = readBlock(input);
-		blockD = readBlock(input);
 		if (feof(input)) break;
+
+		blockB = readBlock(input);
+		if (feof(input)) {//до кратности
+			blockB = blockC = blockD = 65536;
+		}
+
+		blockC = readBlock(input);
+		if (feof(input)) {
+			blockC = blockD = 65536;
+		}
+
+		blockD = readBlock(input);
+		if (feof(input)) {
+			blockD = 65536;
+		}
+
 
 		for (int round = 0; round < 8; round++) {
 			blockA = (blockA * key[round][0]) % 65537;
@@ -52,7 +141,7 @@ void IDEA::coding(char * source, char * out, int * bigKey){
 			blockC = (blockC + key[round][2]) % 65536;
 			blockD = (blockD * key[round][3]) % 65537;
 
-			wchar_t t1, t2;
+			unsigned int t1, t2;
 			t1 = blockA^blockC;
 			t2 = blockB^blockD;
 
@@ -76,13 +165,24 @@ void IDEA::coding(char * source, char * out, int * bigKey){
 		//9й раунд - окончательное преобразование
 		blockA = (blockA * key[8][0]) % 65537;
 		blockB = (blockB + key[8][1]) % 65536;
-		blockC = (blockC * key[8][2]) % 65536;
+		blockC = (blockC + key[8][2]) % 65536;
 		blockD = (blockD * key[8][3]) % 65537;
 
-		fwrite(&blockA, 2, 1, output);
-		fwrite(&blockB, 2, 1, output);
-		fwrite(&blockC, 2, 1, output);
-		fwrite(&blockD, 2, 1, output);
+
+
+		fwrite(&blockA, sizeof(wchar_t), 1, output);
+		if (decode && blockB == 65536) //т.е. это добавлено про кодировании
+			break;
+
+		fwrite(&blockB, sizeof(wchar_t), 1, output);
+		if (decode && blockC == 65536) 
+			break;
+
+		fwrite(&blockC, sizeof(wchar_t), 1, output);
+		if (decode && blockD == 65536) 
+			break;
+
+		fwrite(&blockD, sizeof(wchar_t), 1, output);
 	}
 
 
@@ -90,5 +190,12 @@ void IDEA::coding(char * source, char * out, int * bigKey){
 	fclose(output);
 }
 
-void IDEA::decoding(char * source, char * out, int * bigKey){
+void IDEA::coding(char * source, char * out, int * bigKey) {
+	code(source, out, bigKey, false);
+	std::cout << "\ncoded\n";
+}
+
+void IDEA::decoding(char * source, char * out, int * bigKey) {
+	code(source, out, bigKey, true);
+	std::cout << "\ndecoded\n";
 }
--- cryptoIDEA/Source.cpp
+++ cryptoIDEA/Source.cpp
@@ -11,16 +11,40 @@ using namespace std;
 функцию раунда
 считывание из файла побитово(?)
 обработка сдвига влево: новый индекс + сдвиг*номер сдвига % размера = старый индекс
+коррел€ци€ = 1/N SUM( (2xi-1)(2yi-1) ) ~ 0
 */
+
+wchar_t evclid(int a, wchar_t b) {
+	//a*x + b*y = 1
+	wchar_t buf = a;
+	int q, r, x, x1, x2, y, y1, y2;
+	x2 = 1, x1 = 0, y2 = 0, y1 = 1;
+	while (b > 0) {
+		q = a / b;//
+		r = a - q * b;//
+		x = x2 - q * x1;//
+		y = y2 - q * y1;//
+		a = b;//
+		b = r;//
+		x2 = x1;//
+		x1 = x;//
+		y2 = y1;//
+		y1 = y;//
+			   //std::cout << " a= " << a << " b= " << b << " q= " << q << " x= " << x << " y= " << y << " r= " << r << " x2= " << x2 << " y2= " << y2 << std::endl;
+	}
+	if (y2 < 0) y2 = y2 + buf;
+	return y2;//
+}
 
 void main(int argc, char* argv[]) {
 	IDEA idea;
 	srand(time(NULL));
 	int mas[SIZE];//наш ключ
+	cout << "key: ";
 	for (int i = 0; i < SIZE; i++) {
 		mas[i] = rand() % 2;
-		//cout << mas[i];
-		//if (i % 4 == 3) cout << " ";
+		cout << mas[i];
+		if (i % 4 == 3) cout << " ";
 		//переводит число в дес€тичное
 		//a = a << 1;
 		//a = a + mas[i];
@@ -29,11 +53,31 @@ void main(int argc, char* argv[]) {
 		//	a = 0;
 		//}
 	}
-	idea.coding("text.txt", "out.bin", mas);
 	cout << endl;
+
+	idea.coding(argv[1], argv[2], mas);
+
+	idea.decoding(argv[2], argv[3], mas);
+
+	//unsigned int abc = 65002 * 40950;
+//	abc = abc % 65537;
+	wchar_t abc = 2550 ;
+	cout << abc << endl;
+	cout << hex << (wchar_t)abc << endl;
+	cout << hex << abc << endl;
+	/*FILE *f;
+	fopen_s(&f, "testInt.txt", "wb");
+	fwrite(&abc, sizeof(wchar_t), 1, f);
+	fclose(f);
+	abc = 0;
+	fopen_s(&f, "testInt.txt", "rb");
+	fread(&abc, sizeof(unsigned char), 1, f);
+	cout << abc << endl;
+	fclose(f);*/
 
 
 
 
 	system("PAUSE");
 }
+
